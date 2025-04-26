2955dd3f80e18f639d723e969c42e8152fb23b06
Adam Finlayson <amf@adamfinlayson.com>
2025-04-26 18:57:52 -0500

    chore: config: scripts

--- bin/task.sh
+++ bin/task.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat plan.md | claude \
-"Given the provided plan, move the first queued task to dequeued. Complete that task. Do not move it to verified."
+"Given the provided plan, move the first queued task to dequeued. Complete that task. Leave the task in the dequeued state. Do not move it to verified."
